199fce9a29393bc6d92be0eac34f7455fa611d8c
trinitipsy <jankovic-tamara@hotmail.com>
2019-11-16 21:31:12 +0100

    Add LandingPage component

---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Greeting from '././Greeting';
+import AboutSection from './AboutSection';
+
+const LandingPage = () => (
+    <Fragment>
+        <Greeting />
+        <AboutSection />
+    </Fragment>
+)
+
+export default LandingPage;
