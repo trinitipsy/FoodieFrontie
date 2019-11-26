291d1fce06af67bbf90766cb99ad5e7e1e8a7c37
trinitipsy <jankovic-tamara@hotmail.com>
2019-11-26 15:06:36 +0100

    Send message to backend

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,31 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Form, Message, Grid,Button } from 'semantic-ui-react';
+import { Form, Message, Grid, Button } from 'semantic-ui-react';
 import Text from './Text';
+import axios from '../API/AxiosInstance';
 
-const Footer = () => (
-  <StyledWrapper>
-    <Grid>
-      <Grid.Column width={10}>
-        <Text><b>Contact us</b></Text>
-        <Text>Phone: <i>032/433-324</i></Text>
-        <Text>Email: <i>FoodSetGo@gmail.com</i></Text>
-        <Text>Address: <i>Stepe Stepanovica 16</i></Text>
-        <Text>Follow us on   <button className="ui facebook grey button">
-          <i className="facebook icon"></i>
-          Facebook</button></Text>
-      </Grid.Column>
-      <Grid.Column floated="right" width={6}>
-        <Text>Or send us direct message</Text>
-        <Form success>
-          <Form.Input placeholder='Email: joe@schmoe.com' />
-          <Message visible content="Your message here..." />
-          <Button inverted color='black' size='big'>Submit</Button>
-        </Form>
-      </Grid.Column>
-    </Grid>
-  </StyledWrapper>
-)
+const Contact = ({ navigate }) => {
+  const [userMessage, setUserMessage] = useState({
+    email: '',
+    message: ''
+  });
+
+  const handleChange = (event) => {
+    userMessage[event.target.name] = event.target.value;
+    setUserMessage(userMessage);
+  };
+
+  const submit = (event) => {
+    const response = axios.post(
+      'send-message',
+      userMessage,
+      { headers: { 'Content-Type': 'application/json' } }
+    );
+
+    response.then(value => {
+      console.log(value.data);
+      navigate('/home');
+    });
+    event.preventDefault();
+  };
+
+  return (
+    <StyledWrapper>
+      <Grid>
+        <Grid.Column width={10}>
+          <Text><b>Contact us</b></Text>
+          <Text>Phone: <i>032/433-324</i></Text>
+          <Text>Email: <i>FoodSetGo@gmail.com</i></Text>
+          <Text>Address: <i>Stepe Stepanovica 16</i></Text>
+          <Text>Follow us on   <button className="ui facebook grey button">
+            <i className="facebook icon"></i>
+            Facebook</button></Text>
+        </Grid.Column>
+        <Grid.Column floated="right" width={6}>
+          <Text>Or send us direct message</Text>
+          <Form success onSubmit={submit} >
+            <Form.Input name='email' type="text" placeholder='Email: joe@schmoe.com' onChange={handleChange} />
+            <Form.Input name='message' type="text" placeholder='Your message here' onChange={handleChange} />
+            <Button inverted color='black' size='big'>Submit</Button>
+          </Form>
+        </Grid.Column>
+      </Grid>
+    </StyledWrapper>
+  )
+};
 const StyledWrapper = styled.div`
   background-image: url("sparkle.jpg");
 	background-repeat: no-repeat;
@@ -33,4 +60,4 @@ const StyledWrapper = styled.div`
 	padding: 80px;
   `;
 
-export default Footer;
+export default Contact;
